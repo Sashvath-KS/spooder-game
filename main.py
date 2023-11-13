af775eea42c2757b003972bcb369aff19f7dc504
Sashvath krishnan Sriram <sashvath@Sashvaths-MacBook-Air.local>
2023-11-13 10:48:59 +0530

    completed the menu

--- main.py
+++ main.py
@@ -1,171 +1,132 @@
-import pygame,sys,random
+import pygame,sys
+import car_game #imports the car game from another file
 
-#to start the game
+#to start pygame
 pygame.init()
-game_active=False   #to check if the game if the running
-game_started=False  #to check if the game started
 
-#to create a window
-window=pygame.display.set_mode((960,640))
-pygame.display.set_caption('spooder game')
+#window and its attributes
+window_width=960
+window_height=503
+window_size=(window_width,window_height)
+window=pygame.display.set_mode(window_size)
 
-#game start screen
-start_screen=pygame.image.load('assets/start_screen.jpg').convert()
-window.blit(start_screen,(160,0))
-pygame.display.update()
+#menu background music
+bgm=pygame.mixer.Sound('assets/menu_assets/menu_music.mp3')
+bgm.play(loops=-1)
 
-#game background and background music
-background=pygame.image.load('assets/background2.jpeg').convert()
-music=pygame.mixer.Sound('assets/sundrive.mp3')
-music.play(loops=-1)
-
-#sound effects
-death=pygame.mixer.Sound('assets/roblox-death-sound-effect.mp3')
-explosion=pygame.mixer.Sound('assets/explosion.wav')
-
-#platform rect
-platform=pygame.image.load('assets/platform2.png').convert()
-platform_rect=platform.get_rect(topleft=(0,580))
-
-#player frames
-spooderR=pygame.image.load('assets/spooderR.png').convert_alpha()
-spooderL=pygame.image.load('assets/spooderL.png').convert_alpha()
-spooder_moveL=pygame.image.load('assets/spooder_moveL.png').convert_alpha()
-spooder_moveR=pygame.image.load('assets/spooder_moveR.png').convert_alpha()
-spooder_move=[spooderL,spooderR,spooder_moveL,spooder_moveR]
-spooder_index=1
-
-#player rect and attributes
-spooder_rect=spooder_move[spooder_index].get_rect(midbottom=(0,590))
-health=100
-velocity_player=15
-score=0
-
-#nuke rect and attributes
-nuke=pygame.image.load('assets/nuke2.png').convert_alpha()
-velocity_nuke=5
-g=0.2
-nuke_list=[]
-explosion_cloud=pygame.image.load('assets/explosion.png')
-
-#timer which activates every 0.7 seconds to spawn a nuke
-nuke_timer=pygame.USEREVENT+1
-pygame.time.set_timer(nuke_timer,700)
-
-#manages nuke list, collisions and nuke spawn
-def nuke_fall(nuke_list):
-    global health
-    if nuke_list:
-        #nuke parameters contains [nuke rect , velocity , collision checker(T/F)]
-        for nuke_parameters in nuke_list:
-            #moves the nuke
-            nuke_parameters[0].y+= nuke_parameters[1]
-            nuke_parameters[1]+=g
-            #collisions
-            collided=nuke_parameters[2]
-            if nuke_parameters[0].colliderect(spooder_rect) and not(collided):
-                health-=20
-                nuke_parameters[2]=True
-                explosion.play()
-                window.blit(explosion_cloud,nuke_parameters[0].topleft)
-            window.blit(nuke,nuke_parameters[0])
-        #removes exploded nukes and nukes that missed from the nuke list
-        nuke_list=[nuke_parameters for nuke_parameters in nuke_list if nuke_parameters[0].y<550 and not(nuke_parameters[2])]
-
-        return nuke_list
-    else:return []
-
-#game fps controller
+#to regulate framerate
 clock=pygame.time.Clock()
 
-#score count and health
-font=pygame.font.Font(None,50)
-def disp_score(score):
-    score_surf=font.render('score:'+str(score),False,'pink')
-    score_rect=score_surf.get_rect(center=(480,30))
-    window.blit(score_surf,score_rect)
+#button class to re-use the logic for clicking
+class Button:
+    #init function is given to get the attributes of the button
+    def __init__(self,path1,position,path2) -> None:
+        self.image1=pygame.image.load(path1).convert_alpha()
+        self.rect1=self.image1.get_rect(topleft=position)
+        self.image2=pygame.image.load(path2).convert_alpha()
+        self.rect2=self.image2.get_rect(topleft=position)
 
-def disp_health():
-    health_surf=font.render('health:'+str(health),False,'red')
-    health_rect=health_surf.get_rect(center=(100,30))
-    window.blit(health_surf,health_rect)
-
-#game end screen
-end=pygame.image.load('assets/die.png').convert()
-
-#game loop
-while True:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT: #to quit the game
-            pygame.quit()
-            sys.exit()
-
-        elif event.type==pygame.KEYUP and not(game_active): #to start/restart the game
-            pygame.time.delay(500)
-            game_active=True
-            health=100
-            score=0
-
-        elif event.type==nuke_timer and game_active:    #to periodically spawn nukes
-            #whenever the timer returns True, a nuke is added to the nuke list
-            nuke_list.append(
-                [nuke.get_rect(topleft=(random.randint(0,960),0)),velocity_nuke,False]
-                )
-            score+=10
-
-    if game_active: #the actual game part
-        game_started=True
-        window.blit(background,(0,0))
-        window.blit(platform,platform_rect)
-        
-        #to get what key the player pressed
-        #it is a dictionary like object with bool values
-        keys=pygame.key.get_pressed()
-
-        #to move the player
-        if keys[pygame.K_LEFT]:
-            spooder_rect.left-=velocity_player
-            spooder_index=2
-            window.blit(spooder_moveL,spooder_rect)
-        
-        elif keys[pygame.K_RIGHT]:
-            spooder_rect.left+=velocity_player
-            spooder_index=3
-            window.blit(spooder_moveR,spooder_rect)
-        
-        #player idle position
+    #drawing , hovering and clicking logic
+    def draw(self):
+        mouse_pos=pygame.mouse.get_pos()
+        if self.rect1.collidepoint(mouse_pos):
+            window.blit(self.image2,self.rect2)
+            if True in pygame.mouse.get_pressed():
+                return True
         else:
-            if spooder_index in (2,0):
-                spooder_index=0
-                window.blit(spooderL,spooder_rect)
-            
-            elif spooder_index in (1,3):
-                spooder_index=1
-                window.blit(spooderR,spooder_rect)
+            window.blit(self.image1,self.rect1)
+            return False
 
-        #to stop the player from exiting the screen
-        if spooder_rect.left>960:
-            spooder_rect.right=0
-        
-        elif spooder_rect.right<0:
-            spooder_rect.left=960
-        
-        #to update nuke_list, health and score
-        nuke_list=nuke_fall(nuke_list)
-        disp_health()
-        disp_score(score)
-        
-        #to check if player is dead
-        if health==0:
-            game_active=False
-            death.play()
+# opening screen of the game
+def opening_screen():
     
-    #game ending screen
-    elif not(game_active) and game_started:
-            window.fill('black')
-            window.blit(end,(154.5,83.5))
-            disp_score(score)
+    background=pygame.image.load('assets/menu_assets/background_1.png').convert_alpha()
     
-    #to update the screen every frame and to control frame rate
-    pygame.display.update()
-    clock.tick(70)
+    #to animate the button by changing the image
+    b_index=0
+    b_flag=True
+    
+    #button attributes and button definition 
+    button_xpos=385
+    button_ypos=210
+    button1=Button('assets/menu_assets/start2.png',(button_xpos,button_ypos),'assets/menu_assets/start1.png')
+    button2=Button('assets/menu_assets/start3.png',(button_xpos,button_ypos),'assets/menu_assets/start1.png')
+    
+    #to check if the button was clicked
+    clicked=False
+    
+    #main loop for the opening screen
+    while not(clicked):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT: #to quit the game
+                pygame.quit()
+                sys.exit()
+        
+        window.blit(background,(0,0))
+        
+        #button animation
+        #whenever b_index becomes an int, the b_flag becomes T/F 
+        b_index+=0.1
+        if int(b_index):
+            b_flag=not(b_flag)
+            b_index=0
+        if b_flag:
+            clicked=button1.draw()
+        else:
+            clicked=button2.draw()
+        
+        #to update screen and to control fps
+        pygame.display.update()
+        clock.tick(60)
+    
+    #to change the screen to main menu
+    if clicked:return main_menu()
+
+#main menu of the game
+def main_menu():
+    
+    background=pygame.image.load('assets/menu_assets/background_1.png').convert_alpha()
+    
+    #to create 4 buttons
+    button1=Button('assets/menu_assets/button_spooder-car.png',(115,100),'assets/menu_assets/button_spooder-car(1).png')
+    button2=Button('assets/menu_assets/button_kalahalla.png',(650,100),'assets/menu_assets/button_kalahalla(1).png')
+    button3=Button('assets/menu_assets/button_flappy-thaav.png',(115,300),'assets/menu_assets/button_flappy-thaav(1).png')
+    button4=Button('assets/menu_assets/button_back.png',(650,300),'assets/menu_assets/button_back(1).png')
+    ##a=0;print(a)
+    #main loop of main menu
+    while True:
+        for event in pygame.event.get():    #to quit the game
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        window.blit(background,(0,0))
+
+        #to move to the actual game based on the button clicked by the user
+        if button1.draw():
+            bgm.stop()
+            return car_game.spooder_car()
+        
+        elif button2.draw():
+            print('coming in next update')
+        
+        elif button3.draw():
+            print('coming in next update')
+        
+        elif button4.draw():
+            return opening_screen()
+        
+        #to update the screen and to control fps
+        pygame.display.update()
+        ##clock.tick(60)
+        ##a+=1;print(a)
+        
+
+flag=opening_screen()   #to check if user wants to return to main menu
+
+#this loop allows the player to return to main menu at any time
+while flag:
+    pygame.init()
+    window=pygame.display.set_mode(window_size)
+    bgm.play(loops=-1)
+    main_menu()
